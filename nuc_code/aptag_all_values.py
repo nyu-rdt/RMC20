564deacf7b15d4670d9bf502b854f1b0551f1d1a
Junming-Qiu <jq741@gmail.com>
2019-11-23 14:48:57 -0500

    more comments

--- nuc_code/aptag_all_values.py
+++ nuc_code/aptag_all_values.py
@@ -1,58 +1,12 @@
 from __future__ import division
 from __future__ import print_function
-from argparse import ArgumentParser
 import cv2
 import apriltag
 import numpy
 import math
 
-
-# Draws an overlay of a cube over the apriltag
-def draw_pose(overlay, camera_params, tag_size, pose, z_sign=1):
-    opoints = numpy.array([
-        -1, -1, 0,
-        1, -1, 0,
-        1, 1, 0,
-        -1, 1, 0,
-        -1, -1, -2 * z_sign,
-        1, -1, -2 * z_sign,
-        1, 1, -2 * z_sign,
-        -1, 1, -2 * z_sign,
-    ]).reshape(-1, 1, 3) * 0.5 * tag_size
-
-    edges = numpy.array([
-        0, 1,
-        1, 2,
-        2, 3,
-        3, 0,
-        0, 4,
-        1, 5,
-        2, 6,
-        3, 7,
-        4, 5,
-        5, 6,
-        6, 7,
-        7, 4
-    ]).reshape(-1, 2)
-
-    fx, fy, cx, cy = camera_params
-
-    K = numpy.array([fx, 0, cx, 0, fy, cy, 0, 0, 1]).reshape(3, 3)
-
-    rvec, _ = cv2.Rodrigues(pose[:3, :3])
-    tvec = pose[:3, 3]
-
-    dcoeffs = numpy.zeros(5)
-
-    ipoints, _ = cv2.projectPoints(opoints, rvec, tvec, K, dcoeffs)
-
-    ipoints = numpy.round(ipoints).astype(int)
-
-    ipoints = [tuple(pt) for pt in ipoints.reshape(-1, 2)]
-
-    for i, j in edges:
-        cv2.line(overlay, ipoints[i], ipoints[j], (0, 255, 0), 1, 16)
-
+#All distance and lengths in meters
+#All angles out in degrees
 
 '''
 Converts rotation matrix into radians and degrees
@@ -64,9 +18,7 @@ ex. Rx = [1, 0, 0]
         [0, sin(xangle), cos(xangle)]
 Function taken from: https://www.learnopencv.com/rotation-matrix-to-euler-angles/
 '''
-
-
-def rotation_matrix_to_euler_angles(R, id):
+def rotation_matrix_to_euler_angles(R, tag_id):
     sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
 
     singular = sy < 1e-6
@@ -75,30 +27,39 @@ def rotation_matrix_to_euler_angles(R, id):
         x = math.atan2(R[2, 1], R[2, 2])
         y = math.atan2(-R[2, 0], sy)
         z = math.atan2(R[1, 0], R[0, 0])
+        
     else:
         x = math.atan2(-R[1, 2], R[1, 1])
         y = math.atan2(-R[2, 0], sy)
         z = 0
 
-    if id == 0:
+    if tag_id == 0:
         return numpy.array([math.degrees(x), math.degrees(y) + 180, math.degrees(z)])
-    elif id == 1 or id == 2:
+
+    elif tag_id == 1 or tag_id == 2:
         return numpy.array([math.degrees(x), math.degrees(y) + 90, math.degrees(z)])
-    elif id == 3 or id == 4:
+
+    elif tag_id == 3 or tag_id == 4:
         return numpy.array([math.degrees(x), math.degrees(y), math.degrees(z)])
-    elif id == 5 or id == 6:
+
+    elif tag_id == 5 or tag_id == 6:
         return numpy.array([math.degrees(x), math.degrees(y) + 270, math.degrees(z)])
-    elif id == -1:
+
+    elif tag_id == -1:
         return y
 
-
-# Angular rotation of each apriltag
+'''
+Angular rotation of each apriltag
+position = position matrix
+Output: angle in degrees centered at the center of the camera. Left bound angles are negative (0, -90) degrees
+Right bound angles are positive (0, 90) degrees
+'''
 def find_angular_rotation(position):
     angle = math.atan(position[0] / position[2])
     return math.degrees(angle)
 
 
-# From 0 - 180 degrees
+# Converts position into absolute angular position in degrees (0 - 180) degrees
 def find_absolute_angular_rotation(position):
     relangle = math.degrees(math.atan(position[0] / position[2]))
     if relangle == 0:
@@ -109,16 +70,19 @@ def find_absolute_angular_rotation(position):
 
     elif relangle > 0:
         absangle = 90 + math.fabs(relangle)
+
     return absangle
 
 
 # Average angle of 2 border apriltags
+# Center apriltags are ignored
 def adjusted_angular_rotation(storedangles):
     length = len(storedangles)
 
     if length > 2:
         ang1 = max(storedangles)
         ang2 = min(storedangles)
+
     else:
         ang1 = storedangles[0]
         ang2 = storedangles[1]
@@ -127,11 +91,18 @@ def adjusted_angular_rotation(storedangles):
 
     if (ang1 + ang2) / 2 < 90:
         return -1 * adjusted_rotation_angle
+
     else:
         return adjusted_rotation_angle
 
-
-# Center enter position of robot
+'''
+Finds center position of robot using position matrix, rotation matrix, and tag id
+diag_length is distance from center of robot to apriltag in meters
+need_angle is angle of diag_length to line perpendicular to side of robot
+Works for test robot, which is a rectangle with 2 apriltags on each of the long sides and one apriltag 
+for each of the short sides. Returns center position (x,y), which is the position of the robot from a bird's view
+perspective. x is the horizontal position from the center of the camera and y is the vertical position
+'''
 def center_position(position, rotation_matrix, id):
     relative_orientation = rotation_matrix_to_euler_angles(rotation_matrix, -1)
 
@@ -141,6 +112,7 @@ def center_position(position, rotation_matrix, id):
         relative_orientation = math.fabs(relative_orientation)
         x_add = diag_length * math.sin(relative_orientation)
         y_add = diag_length * math.cos(relative_orientation)
+
         if relative_orientation < 0:
             x_add *= -1
 
@@ -149,15 +121,18 @@ def center_position(position, rotation_matrix, id):
 
         relative_orientation = math.fabs(relative_orientation)
         if id == 2 or id == 6:
+
             needed_angle = 0.785398
             if relative_orientation > math.radians(45):
                 new_angle = relative_orientation - needed_angle
                 x_add = -1 * diag_length * math.cos(new_angle)
                 y_add = diag_length * math.sin(new_angle)
+
             elif math.radians(-45) < relative_orientation < math.radians(45):
                 new_angle = needed_angle - relative_orientation
                 x_add = diag_length * math.cos(new_angle)
                 y_add = diag_length * math.sin(new_angle)
+
             elif relative_orientation < math.radians(-45):
                 new_angle = relative_orientation - needed_angle
                 x_add = diag_length * math.cos(new_angle)
@@ -165,14 +140,17 @@ def center_position(position, rotation_matrix, id):
 
         elif id == 5 or id == 1:
             needed_angle = 0.785398
+
             if relative_orientation > math.radians(45):
                 new_angle = relative_orientation - needed_angle
                 x_add = -1 * diag_length * math.cos(new_angle)
                 y_add = 1 * diag_length * math.sin(new_angle)
+
             elif math.radians(-45) < relative_orientation < math.radians(45):
                 new_angle = needed_angle - relative_orientation
                 x_add = diag_length * math.cos(new_angle)
                 y_add = diag_length * math.sin(new_angle)
+
             elif relative_orientation < math.radians(-45):
                 new_angle = relative_orientation - needed_angle
                 x_add = diag_length * math.cos(new_angle)
@@ -189,13 +167,17 @@ def avgposition(length):
     total = 0
     count = 0
     print(length)
+
     for measurement in length:
         total += measurement
         count += 1
+
     average = total / count
+
     return average
 
 
+#Prints all data determined by the program
 def debug_log(detection, pose, init_error, final_error, rotation_matrix, position, num_detections, i):
     # Converts rotation matrix of individual apriltag into degrees and radians
     print('Rotation in Degrees (pitch,yaw,roll)', rotation_matrix_to_euler_angles(rotation_matrix, detection.tag_id))
@@ -214,9 +196,13 @@ def debug_log(detection, pose, init_error, final_error, rotation_matrix, positio
 
 
 def main():
-    # Values come from running camera calibration file (fx, fy, cx,cy).
-    # Camera should be calibrated for each new computer.
-    # camera_params = [1.01446618 * 10 ** 3, 1.02086461 * 10 ** 3, 6.09583146 * 10 ** 2, 3.66171174 * 10 ** 2]
+    '''
+    Values come from running camera calibration file (fx, fy, cx,cy).
+    Camera should be calibrated for each new computer and new camera.
+    If the projected green box for the apriltag does not have sudden jumps when you turn the tag slightly,
+    then the calibration is correct.
+    '''
+    #camera_params = [1.01446618 * 10 ** 3, 1.02086461 * 10 ** 3, 6.09583146 * 10 ** 2, 3.66171174 * 10 ** 2]
     camera_params = [1.31239907 * 10 ** 3, 1.31169637 * 10 ** 3, 9.23293617 * 10 ** 2, 5.49707267 * 10 ** 2]
 
     # Set value for camera
@@ -227,6 +213,7 @@ def main():
 
     detector = apriltag.Detector()
 
+    # Apriltag detection loop
     while True:
         # Stores values of each detected tag for final output
         stored_angles = []
@@ -246,6 +233,8 @@ def main():
         print('Detected {} tags.\n'.format(num_detections))
 
         tag_count = -1
+
+        # Detection for each apriltag
         for i, detection in enumerate(detections):
 
             # Returns pose matrix
@@ -277,16 +266,17 @@ def main():
 
             overlay = frame // 2 + dimg[:, :, None] // 2
 
-            # Draws all overlays before going to next frame
-
+            # Draws all overlays before going to next frame. Must import cv2 in apriltag module
+            # _draw_pose(overlay, camera_params, tag_size, pose, z_sign=1)
             for det in range(num_detections):
-                draw_pose(overlay, camera_params, 0.17, pose, z_sign=1)
+                apriltag._draw_pose(overlay, camera_params, 0.17, pose, z_sign=1)
 
             cv2.imshow(window, overlay)
             cv2.waitKey(27)
 
         # print('stored_angles', stored_angles)
 
+        # Final output
         if num_detections > 1:
             print('Adjusted Rotation Angle: ', adjusted_angular_rotation(stored_angles))
         elif num_detections == 1:
