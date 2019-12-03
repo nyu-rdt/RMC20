1f125657d09435ae0491e8c1d01e2cb6e8011ace
Junming-Qiu <jq741@gmail.com>
2019-12-03 12:51:02 -0500

    better turtle and measurements

--- nuc_code/aptag_optimized.py
+++ nuc_code/aptag_optimized.py
@@ -6,10 +6,11 @@ import numpy
 import math
 import time
 import turtle
+import copy
 
 
 # All distance and lengths in meters
-# All angles out in degrees
+# All angles output converted to degrees
 
 '''
 Converts rotation matrix into radians and degrees
@@ -21,8 +22,6 @@ ex. Rx = [1, 0, 0]
         [0, sin(xangle), cos(xangle)]
 Function taken from: https://www.learnopencv.com/rotation-matrix-to-euler-angles/
 '''
-
-
 def rotation_matrix_to_euler_angles(R, tag_id):
     sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
 
@@ -41,24 +40,27 @@ def rotation_matrix_to_euler_angles(R, tag_id):
     # Set which ids are on which side
 
     # Back
-    if tag_id == 2:
+    if tag_id == 3:
         return numpy.array([math.degrees(x), math.degrees(y), math.degrees(z)])
 
     # Right
-    elif tag_id == 9 or tag_id == 10:
+    elif tag_id == 4 or tag_id == 5:
         return numpy.array([math.degrees(x), math.degrees(y) + 90, math.degrees(z)])
 
     # Front
-    elif tag_id == 3:
+    elif tag_id == 0:
         return numpy.array([math.degrees(x), math.degrees(y) + 180, math.degrees(z)])
 
     # Left
-    elif tag_id == 0 or tag_id == 1:
+    elif tag_id == 1 or tag_id == 2:
         return numpy.array([math.degrees(x), math.degrees(y) + 270, math.degrees(z)])
 
     # Call function without predefined rotations
     elif tag_id == -1:
         return y
+
+    elif tag_id == 6:
+        return numpy.array([math.degrees(x), math.degrees(y), math.degrees(z)])
 
 
 '''
@@ -67,8 +69,6 @@ position = position matrix
 Output: angle in degrees centered at the center of the camera. Left bound angles are negative (0, -90) degrees
 Right bound angles are positive (0, 90) degrees
 '''
-
-
 def find_center_angular_rotation(position):
     angle = math.atan(position[0] / position[1])
     return math.degrees(angle)
@@ -84,19 +84,23 @@ meter is offset by. meter_length_adjustment = 1 - offset
 Works for test robot, which is a rectangle with 2 apriltags on each of the long sides and one apriltag 
 for each of the short sides. Returns center position (x,y), which is the position of the robot from a bird's view
 perspective. x is the horizontal position from the center of the camera and y is the vertical position
+
+Variables: diagonal_length, default_angle, meter_length_adjustment
+diagonal_length is the distance from the center of the apriltag to the center of the robot
+default_angle is the angle the apriltag makes with the diagonal line
+meter_length_adjustment is the scaling factor. This can be set for each individual tag depending on how precisely you 
+want to calibrate the measurements
 '''
-
-
 def center_position(position, rotation_matrix, id):
 
     relative_orientation = rotation_matrix_to_euler_angles(rotation_matrix, -1)
-    diagonal_length = 0.212
-    default_angle = math.radians(48.2)
-    meter_length_adjustment = 0.98
+    diagonal_length = 0.33234
+    default_angle = math.radians(45)
+    meter_length_adjustment = 1
 
     # Right Tag
-    if id == 1:
-        total_angle = math.pi - default_angle + relative_orientation
+    if id == 5 or id == 2 or id == 6:
+        total_angle = math.pi - default_angle - relative_orientation
         print(diagonal_length * math.cos(total_angle))
         print(math.degrees(total_angle))
 
@@ -110,12 +114,13 @@ def center_position(position, rotation_matrix, id):
         return [numpy.float(x_position * meter_length_adjustment), numpy.float(y_position * meter_length_adjustment)]
 
     # Left Tag
-    if id == 0:
+    if id == 4 or id == 1:
         total_angle = default_angle + relative_orientation * -1
 
     # Only one Tag
-    elif id == 3 or id == 2:
-        diagonal_length = .245
+    elif id == 0 or id == 3:
+        meter_length_adjustment = 1.02
+        diagonal_length = .50
         total_angle = relative_orientation + 90
 
     if math.degrees(total_angle) > 90:
@@ -126,6 +131,34 @@ def center_position(position, rotation_matrix, id):
         y_position = position[2] + diagonal_length * math.sin(total_angle)
 
     return [numpy.float(x_position * meter_length_adjustment), numpy.float(y_position * meter_length_adjustment)]
+
+
+'''
+Takes the stored_pose list and finds the index of the smallest angle in the list.
+Creates a list of rotation matrices, then makes a list of the angles. These will keep the original index order since
+the lists are appended. A deep copied list and a sorted list are made for the orientation_list. The first value 
+(smallest value) is compared to the copied (original) list and the index is found.
+'''
+def find_smallest_rotation_index(stored_pose):
+    rotation_matrix_list = []
+
+    for stored_index in range(len(stored_pose)):
+        rotation_matrix = numpy.array([stored_pose[stored_index][0][:3],
+                                       stored_pose[stored_index][1][:3],
+                                       stored_pose[stored_index][2][:3]])
+        rotation_matrix_list.append(rotation_matrix)
+
+    orientation_list = []
+    for matrix in rotation_matrix_list:
+        angle = math.fabs(rotation_matrix_to_euler_angles(matrix, -1))
+        orientation_list.append(angle)
+
+    original_orientation_list = copy.deepcopy(orientation_list)
+
+    orientation_list.sort()
+    smallest_angle_index = original_orientation_list.index(orientation_list[0])
+
+    return smallest_angle_index
 
 
 # Prints all data determined by the program
@@ -140,33 +173,50 @@ def debug_log(detection, pose, init_error, final_error, num_detections, i):
     print()
 
 
-# Performs all calculations
+# Performs/calls for all calculations
 def calc_values(stored_pose, stored_id):
-    rotation_matrix = numpy.array([stored_pose[0][0][:3],
-                                   stored_pose[0][1][:3],
-                                   stored_pose[0][2][:3]])
 
-    position = numpy.array([stored_pose[0][0][3:], stored_pose[0][1][3:], stored_pose[0][2][3:]])
+    if len(stored_pose) > 1:
+        smallest_rotation_index = find_smallest_rotation_index(stored_pose)
 
-    orientation = rotation_matrix_to_euler_angles(rotation_matrix, stored_id[0])[1]
+    else:
+        smallest_rotation_index = 0
 
-    center_coords = center_position(position, rotation_matrix, stored_id[0])
+    rotation_matrix = numpy.array([stored_pose[smallest_rotation_index][0][:3],
+                                   stored_pose[smallest_rotation_index][1][:3],
+                                   stored_pose[smallest_rotation_index][2][:3]])
+
+    position = numpy.array([stored_pose[smallest_rotation_index][0][3:],
+                            stored_pose[smallest_rotation_index][1][3:],
+                            stored_pose[smallest_rotation_index][2][3:]])
+
+    orientation = rotation_matrix_to_euler_angles(rotation_matrix, stored_id[smallest_rotation_index])[1]
+
+    center_coords = center_position(position, rotation_matrix, stored_id[smallest_rotation_index])
 
     angular_rotation = find_center_angular_rotation(center_coords)
 
     return rotation_matrix, orientation, center_coords, angular_rotation
 
 
+# Maps coordinates and rotation of robot to turtle.
+# scale adjusted to set distance over travelled pixel ratio
 def turtle_draw(coordinates, orientation):
+    scale = 100
+    turtle.clear()
     turtle.setheading(orientation * -1 + 90)
-    turtle.goto(round(coordinates[0], 2) * 200, round(coordinates[1], 2) * 200)
+    turtle.goto(round(coordinates[0], 2) * scale, round(coordinates[1], 2) * scale)
+
+    turtle.write([round(coordinates[0], 2), round(coordinates[1], 2)],
+                 False, align='left', font=('Arial', 20, 'normal'))
 
 
 def main():
+    # Setup turtle
     turtle.clear()
     turtle.setworldcoordinates(-500, 0, 500, 1000)
     turtle.penup()
-    turtle.speed(2)
+    turtle.speed(5)
     turtle.turtlesize(2, 2, 1)
 
     '''
@@ -174,14 +224,17 @@ def main():
     Camera should be calibrated for each new computer and new camera.
     If the projected green box for the apriltag does not have sudden jumps when you turn the tag slightly,
     then the calibration is correct.
+    Sometimes, if the outputs aren't correct, try calibrating with a higher number of detections. 
+    The better the calibration, the less you need to adjust the scale of the measurements in center_position()
     '''
-    camera_params = [1.01446618 * 10 ** 3, 1.02086461 * 10 ** 3, 6.09583146 * 10 ** 2, 3.66171174 * 10 ** 2]
-    # camera_params = [1.31239907 * 10 ** 3, 1.31169637 * 10 ** 3, 9.23293617 * 10 ** 2, 5.49707267 * 10 ** 2]
+    # camera_params = [1.01446618 * 10 ** 3, 1.02086461 * 10 ** 3, 6.09583146 * 10 ** 2, 3.66171174 * 10 ** 2]
+    camera_params = [1.13681104 * 10 ** 3, 1.13670829 * 10 ** 3, 9.61298528 * 10 ** 2, 5.50718212 * 10 ** 2]
 
     # Set value for camera
     cap = cv2.VideoCapture(0)
 
-    window = 'Camera'
+    # Name for window
+    window = 'Camera Detection Window'
     cv2.namedWindow(window)
 
     detector = apriltag.Detector()
@@ -191,7 +244,6 @@ def main():
         # Stores values of each detected tag for final output
         stored_pose = []
         stored_id = []
-        stored_position = []
 
         success, frame = cap.read(1)
         if not success:
@@ -208,6 +260,7 @@ def main():
         for i, detection in enumerate(detections):
 
             # Returns pose matrix
+            # Set tagsize in meters here
             pose, init_error, final_error = detector.detection_pose(detection, camera_params, tag_size=0.17, z_sign=1)
 
             # debug_log(detection, pose, init_error, final_error, num_detections, i)
@@ -226,20 +279,18 @@ def main():
             cv2.imshow(window, overlay)
             cv2.waitKey(27)
 
-        # print('stored_angles', stored_angles)
-
+        # Outputs only if tags are detected
         if num_detections > 0:
             #time.sleep(1)
             rotation_matrix, orientation, center_coords, angular_rotation = calc_values(stored_pose, stored_id)
 
-            #print('Rotation Matrix:\n', rotation_matrix)
-            #print()
             print('Center Position:\n', center_coords)
             print()
             print('Orientation\n', orientation)
             print()
             print('Angular Rotation:\n', angular_rotation)
             turtle_draw(center_coords, orientation)
+
 
 if __name__ == '__main__':
     main()
