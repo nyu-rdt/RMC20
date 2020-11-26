2fe4d4544e4ca7714f2a19151a15221fdc0be453
DanShafman <dshafman0900@bths.edu>
2020-11-25 22:57:13 -0500

    Minor documentation changes; atlas socket is now fully functional AND documented, good job team :)

--- gcs_code_py/src/socket/src/atlas_socket.py
+++ gcs_code_py/src/socket/src/atlas_socket.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+
+"""
+atlas_socket.py
+
+Takes in byte strings through the ROS topic 'send_buffer' and forwards them through the socket to 
+the identical destination socket on the other side. Concurrently reads in data received through the 
+socket and writes it to the ROS topic 'recv_buffer'. Additionally exchanges a constant stream of
+dummy bytes to check that the connection is still alive. All actions performed at specified rate.
+
+Used for communication between the Ground Control Station and the Server. Keyboard commands are
+sent GCS->Server and robot data is sent Server->GCS.
+"""
+
 import rospy
 from std_msgs.msg import String
 
@@ -6,31 +19,40 @@ import socket, json, os, time
 import threading, Queue
 import struct
 
-# maxsize <= 0 means infinite queue size
-inputBuffer = Queue.Queue(maxsize = -1)
-outputBuffer = Queue.Queue(maxsize = -1)
+# Global constants
+G_TARGET_IP = "192.168.1.2"
+G_TARGET_PORT = 10010
+G_LOCAL_IP = "192.168.1.15"
+G_LOCAL_PORT = 10010
+G_REPORT_RATE = 20                # Rate (in Hz) that connection is checked 
+G_FAILSAFE_THRESHOLD = 60         # How many failed connections until ESTOP is triggered
 
-#publishes output data
-pub = rospy.Publisher("RecvBuffer", String, queue_size=10)
+# Maxsize <= 0 means infinite queue size
+input_buffer = Queue.Queue(maxsize = -1)
+output_buffer = Queue.Queue(maxsize = -1)
+
+# Publishes output data
+pub = rospy.Publisher("recv_buffer", String, queue_size=10)
 
 """
+Puts data from incoming ROS topic to input buffer; callback
 """
-def SendBuffer(data):
-    global inputBuffer
+def send_buffer(data):
+    global input_buffer
     if(len(data.data)>0):
-        inputBuffer.put(data.data)
+        input_buffer.put(data.data)
 
-#subscribes input data
-rospy.Subscriber("SendBuffer", String, SendBuffer)
+# Subscribes to input data
+rospy.Subscriber("send_buffer", String, send_buffer)
 
 ros_dead = False 
 
 class AtlasSocket(threading.Thread):
     """
-        socket class initialization
-        has an I/O buffer, target ip/port, local ip/port, report rate, failsafe_th
+    Socket class initialization
+    Has I/O buffers, target ip/port, local ip/port, report rate, failsafe threshold
     """
-    def __init__(self, threadName, input_buffer, output_buffer, target_ip = '127.0.0.1', target_port = 10010, local_ip = '127.0.0.1', local_port = 10010, report_rate = 20, failsafe_th = 5):
+    def __init__(self, threadName, input_buffer, output_buffer, target_ip = '127.0.0.1', target_port = 10010, local_ip = '127.0.0.1', local_port = 10010, report_rate = 20, failsafe_th = 60):
         self.input_buffer = input_buffer
         self.output_buffer = output_buffer
         self.target_ip = target_ip
@@ -44,7 +66,7 @@ class AtlasSocket(threading.Thread):
         self.failsafe_th = failsafe_th #Fail Safe Threshold, the amount of time the connection is allowed to fail 
         self.report_rate = report_rate
         self.reconnection_flag = False 
-        self.HEARTBEAT_PACKAGE = self.data2bytestring([0x4E, 0x52, 0x00])	#initialize the heartbeat package
+        self.HEARTBEAT_PACKAGE = self.data_to_byte_string([0x4E, 0x52, 0x00])	#initialize the heartbeat package
         #a small amount of data that is sent to check if the connection is still alive
         self.last_data = self.HEARTBEAT_PACKAGE	#default value of last_data is heartbeat package
         super(AtlasSocket, self).__init__(name = threadName)
@@ -54,21 +76,23 @@ class AtlasSocket(threading.Thread):
     """
     def get_data(self): 
         receive_data = bytearray(2500)
-        #tries to read data from a socket
+
+        # Tries to read data from a socket
         try:
             receive_nbytes, receive_addr = self.conn.recvfrom_into(receive_data, 2048)	#read data from socket
         except:
             receive_nbytes = 0
-        #If no data is received, increaase no response counter, This is the fail safee
+
+        # If no data is received, increaase no response counter, This is the fail safee
         if receive_nbytes == 0:
             self.no_response_counter += 1
-            #if the connection fails more than the threshold, break the connection
+            # If the connection fails more than the threshold, break the connection
             if self.no_response_counter >= self.failsafe_th:
+                rospy.loginfo("====== CONNECTION BROKE =====")
                 self.no_response_counter = self.failsafe_th		
                 self.conn_break = True	
                 self.reset_socket()
-                #receive_dict['type'] = 'connection_break'	#make a 'connection_break' data to inform the main thread
-                #self.output_buffer.put(receive_data)		#put dict into output buffer(queue)
+
         #If data received, put the bytes into ouput_buffer       
         else:	
             self.no_response_counter = 0	
@@ -80,58 +104,60 @@ class AtlasSocket(threading.Thread):
     Send data from the rostopic, SendBuffer to the heartbeat_package
     """
     def send_data(self):
-        #handle the no-response situation(reconnection) and send HEARTBEAT_PACKAGE to reconnect to the robot   
+        # Handle the no-response situation(reconnection) and send HEARTBEAT_PACKAGE to reconnect to the robot   
         if self.no_response_counter > 0:			
             self.conn.sendto(self.HEARTBEAT_PACKAGE, (self.target_ip, self.target_port))
             self.reconnection_flag = True
-        #if there is at least one command need to be send in the input buffer(queue), send it
+
+        # If there is at least one command need to be send in the input buffer(queue), send it
         elif not self.input_buffer.empty():
-            #clear the flag of reconnection
+            # Clear the flag of reconnection
             if self.reconnection_flag:
                 self.reconnection_flag = False  
-            #get new command from queue
+            # Get new command from queue
             else:
                 self.last_data = self.input_buffer.get()
             print(len(self.last_data), self.last_data)
             self.conn.sendto(self.last_data, (self.target_ip, self.target_port))
-        #send the heartbeat package if there is not data needed to be sent
+        # Send the heartbeat package if there is not data needed to be sent
         else: 
             self.conn.sendto(self.HEARTBEAT_PACKAGE, (self.target_ip, self.target_port))
         
     """
-    Runs atlas socket 
+    Runs atlas socket; main loop
     """
     def run(self):
         global ros_dead
         ros_dead = rospy.is_shutdown()
-        rate = rospy.Rate(self.report_rate) #10hz
-        #run while ros topic is still active 
+        rate = rospy.Rate(self.report_rate) # 10hz
+
+        # Run while ros topic is still active 
         while not ros_dead:
             ros_dead = rospy.is_shutdown()
-            #if connection is unstable stop sending and recieving 
+            # If connection is unstable stop sending and recieving 
             if not self.hanging:
                 self.get_data() 
                 self.send_data()
-            rate.sleep() #sleep at self.report_rate frequency
+            rate.sleep() # Sleep at self.report_rate frequency
+
         self.conn.close()
         
     """
-        socket_init(self)
-        initializes socket for starting the communication
+    Initializes socket for starting the communication
     """
     def socket_init(self):
-        if self.initialized:	#if socket has been initialized, close it and initialize again
-            self.conn.close()   #close socker 
-            self.initialized = False #clear initialized flag
+        if self.initialized:	                                            # If socket has been initialized, close it and initialize again
+            self.conn.close()                                               # Close socket 
+            self.initialized = False                                        # Clear initialized flag
         try:
-            self.conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	#initialize a socket object
+            self.conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	# Initialize a socket object
             self.conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.conn.bind((self.local_ip, self.local_port))	#bind local ip and port
+            self.conn.bind((self.local_ip, self.local_port))	            # Bind local ip and port
             self.conn.setblocking(0)
         except:
             self.conn.close()
             return
-        self.initialized = True 	#set initialized flag
+        self.initialized = True 	                                        # Set initialized flag
 
     """
     Reset socket to its original state if there is no connection
@@ -144,6 +170,7 @@ class AtlasSocket(threading.Thread):
             self.socket_start()
         self.conn_break = False
         self.no_response_counter = 0
+
     """
     Change the Target info
     """
@@ -174,26 +201,18 @@ class AtlasSocket(threading.Thread):
     """
     Convert data from an integer list into a bytestring e.g.[0x01,0x02,0x03] -> '\x01\x02\x03'
     """
-    def data2bytestring(self, data):
+    def data_to_byte_string(self, data):
         s = ''
         for x in data:
             s += struct.pack('B',x)
         return s
 
     """
-        socket_stop(self)
-        stops socket communication, hanging is True
-
-        socket_start(self) 
-        initializes socket, hanging is False
-    
-    """
-
-    """
-    stop the program if the socket connection is stopped 
+    Stop the program if the socket connection is stopped 
     """
     def socket_stop(self):
         self.hanging = True
+
     """
     Initializes the socket and starts the program
     """
@@ -202,33 +221,31 @@ class AtlasSocket(threading.Thread):
         self.hanging = False
 
 """
-    retrieves data from output buffer topic and writes to the ROS topic
+Retrieves data from output buffer topic and writes to the ROS topic
 """
-def pubThread():
-    global outputBuffer
+def pub_thread():
+    global output_buffer
     global ros_dead
     global pub
 
-    #get and send data until connection is established 
+    # Get and send data until connection is established 
     while not ros_dead:
         try:
-            data = outputBuffer.get(timeout=1) #retrieve data from output buffer topic
-            string_data = str(data) #translate everything to string
-            #rospy.loginfo(data)
-            pub.publish(data = string_data) #publishes data to the ROS topic
+            data = output_buffer.get(timeout=1)
+            string_data = str(data)
+            pub.publish(data = string_data)
         except:
             pass
-        #rospy.loginfo("There are not strangers to love. You know the rules and so do I ~")
     return
 
 if __name__ == '__main__':
     rospy.init_node("atlas_socket", anonymous=False)
-    pubThread = threading.Thread(target = pubThread)
-    pubThread.start()
-    sock = AtlasSocket("",inputBuffer, outputBuffer, target_ip = '192.168.1.2' , local_ip = '192.168.1.15')
+    pub_thread = threading.Thread(target = pub_thread)
+    pub_thread.start()
+    sock = AtlasSocket("",input_buffer, output_buffer, target_ip = G_TARGET_IP, target_port = G_TARGET_PORT, local_ip = G_LOCAL_IP, local_port = G_LOCAL_PORT, report_rate = G_REPORT_RATE, failsafe_th = G_FAILSAFE_THRESHOLD)
     sock.socket_start()
     sock.run()
-    pubThread.join()
+    pub_thread.join()
     #rospy.spin()
 
 
