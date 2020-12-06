3751dd42b37a1b56172146842d2611601a19540b
Alexandra Lachmann <56484536+alachalex@users.noreply.github.com>
2020-12-05 19:22:30 -0500

    Add files via upload

---
+++ gcs_code_py/src/funcs.py
@@ -0,0 +1,170 @@
+keyboard = ['W','A','S','D', ' '] # this will be taken from the keyboard class  
+
+
+def encoder(a):  
+    # a is array of booleans; a: list[bool]
+    # presumably you're actually going to search for the specific key that you need to use to call a function  
+    
+    
+    # right now this is pushing all of the keys it get of the in     
+    #  7 -> 0b0111 -> [f, t, t, t] 
+    return [sum([int(a[i]) << i for i in range(len(a))])]
+ 
+
+def decoder(a):
+    global keyboard
+
+    # 7 -> 0b0111 -> [f, t, t, t] 
+    # inverse [f, t, t, t] 
+    # [ t , t , t , f ] 
+    # ['W','A','S','D'] 
+    # decoder([7]) -> WAS 
+
+
+    # printing a statement for demonstration purposes 
+    # actual functions should just call the relevant subsystems 
+    print("".join(
+        [keyboard[i] if (2**i) & a[0] > 0 else "" for i in range(len(keyboard))]
+    ))
+
+
+def perms(n):
+    for i in range(2 ** n):
+        yield [((2**j) & i) > 0 for j in range(n)]
+
+
+def test_func(keyboard, encoder, decoder):
+    for perm in perms(len(keyboard)):
+        print("".join([str(keyboard[i]) if perm[i] else '' for i in range(len(keyboard))]))
+        decoder(encoder(perm))
+        print("-------")
+
+
+# for perm in perms(len(keyboard)):
+#     #print(perm)
+#     print("".join([keyboard[i] if perm[i] else '' for i in range(len(keyboard))]))
+#     #   print(encoder(perm))
+#     decoder(encoder(perm))
+#     print("-------")
+
+# encoders and decoders 
+
+def encoder_emergency_stop(a):
+    # emergency stop is at 0th index
+    if a[0]:
+        return [1]
+    return [0]
+    
+
+def decoder_emergency_stop(d):
+    # if d[0] is 1, emergency stop
+    # if not, then don't emergency stop
+    # replace print with function calls
+    if d[0]:
+        print("emergency stop")
+    else:
+        print("no emergency stop")
+
+test_func([1], encoder_emergency_stop, decoder_emergency_stop)
+
+def encoder_door(a):
+    # first bit to open, second bit to close
+    if not a[0] ^ a[1]:
+        return [0]
+    if a[0]:
+        return [0b01]
+    if a[1]:
+        return [0b10]
+
+
+def decoder_door(d):
+    # 0b01 means open, 0b10 means close
+    # replace print with function calls
+    if d[0] & 0b01:
+        print("open door")
+    elif d[0] & 0b10:
+        print("close door")
+    else:
+        print('No door action')
+
+test_func([1,1], encoder_door, decoder_door)
+
+def encoder_actuator(a):
+    # first bit to move forward, second bit to move backward
+    if not a[0] ^ a[1]:
+        return [0]
+    if a[0]:
+        return [0b01]
+    if a[1]:
+        return [0b10]
+
+
+def decoder_actuator(d):
+    # 0b01 means forward, 0b10 means backward
+    # replace print with function calls
+    if d[0] & 0b01:
+        print("Move linear actuator forward")
+    elif d[0] & 0b10:
+        print("Move linear actuator backward")
+    else:
+        print('Stop actuator')
+        
+test_func([1,1], encoder_actuator, decoder_actuator)
+
+
+
+def encoder_manual_drive(IIII):
+    # should recieve wasd
+    # a=0 d=1 s=2 w=3   
+    
+    # don't go forward and backwards at the same time
+    I = 1
+    II = 1
+    if IIII[3]: I += 1
+    if IIII[2]: I -= 1
+    # same for left and right
+    if IIII[0]: II += 1
+    if IIII[1]: II -= 1
+    II = II << 3
+    I = I | II
+    return [I]
+
+
+def decoder_manual_drive(d):
+    # will recieve forward, left, back, reverse (WASD)
+    I = d[0] & 0b111000
+    I = I >> 3
+    II = d[0] & 0b000111
+    if I == 0: print( "go leftwards" )
+    if I == 2: print( "go rightwards" )
+
+    if II == 0: print( "go backwards" )
+    elif II == 2: print( "go frontwards")
+    print("decoder manual drive")
+
+test_func(['w','a','s','d'], encoder_manual_drive, decoder_manual_drive)
+
+
+
+def encode_drum_speeds(a):
+    #recieves drum speed 
+    # index 0 of a is positive rotation, index 1 is neg
+    d = 0
+    if a[0]: d += 0b1000
+    elif [1]: d += 0b0010
+    print("drum rotating")
+
+
+def decode_drum_speeds(d):
+    if d[0] & 0b1000: pass #rotate positively
+    if d[0] & 0b0010: pass #rotate in the negative direction
+
+def encoder_arm(a):
+    d = 0
+    if a[0]: d += 0b0100  #arm moves in positive direction
+    elif a[1]: d += 0b0001 #arm moves in negative direction
+    
+    
+def decoder_arm(d):
+    if d[0] & 0b1000: pass
+    elif d[0] & 0b0010: pass
---
+++ gcs_code_py/src/jumptable.py
@@ -0,0 +1,178 @@
+turnfrom KeyMap import Key_library
+
+class Functions:
+    # constructor
+    def __init__(self, keyboard, encoder, decoder, setup, cleanup, num_bytes=0):
+        self.num_bytes = num_bytes
+        self.keyboard = keyboard
+        
+        # func(lst(chr)) -> lst(bool)
+        self.encoder = encoder
+        
+        # func(lst(chr)) -> None
+        self.decoder = decoder
+        
+        # func(bool) -> None
+        self.setup = setup
+        
+        # func(bool) -> None
+        self.cleanup = cleanup
+
+        # lst(tuple(chr, int))
+        self.func_vector = None
+
+    
+class FunctionTable:
+    # constructor
+    def __init__(self):
+        self.curr_index = 0
+        self.compressed_used = 0
+
+        # holds a tuple of a character and an integer
+        # (char, int)
+        self.func_vector = []
+
+        # lst(Functions)
+        self.func_table = []
+
+
+    # destructor
+    def __del__(self):
+        self.curr_index = None
+        self.compressed_used = None
+        self.manage = None
+        self.func_vector = None
+
+ 
+    """
+    Function used to insert a function into func_table by providing its components
+    returns None
+    parameters:
+        num_bytes: int
+        keyboard: Keyboard
+        encoder: func(lst(bool)) -> lst(chr)
+        decoder: func(lst(chr)) -> None
+        setup: func(bool) -> None
+        cleanup: func(bool) -> None
+    """
+    def insert(self, num_bytes, keyboard, encoder, decoder, setup, cleanup):
+        func = Functions(keyboard, encoder, decoder, setup, cleanup, num_bytes)
+        self.insert_function(func)
+
+
+    '''
+    Function used to insert a function into func_table by providing the function
+    returns None
+    parameters:
+        f: Functions
+    '''
+    def insert_function(self, function):
+        self.func_table.append(function)
+        tot = 0
+
+        # casting k to int isn't allowed in python
+        # easy solution would be to change KeyMap from Enum to IntEnum, see how it affects other files first
+        for k in function.keyboard:
+            tot |= 1 << k.value
+    
+        self.compressed_used |= tot
+
+        self.func_vector.append((self.curr_index, tot))
+
+        self.curr_index += 1
+
+
+    def setup(self, sender):
+         for data in self.func_vector:
+            '''
+            Following line may not work in python
+            We are casting the first field of data which is a char as an index
+            We may need to do something special to avoid issues in python
+            '''
+            print(data[0])
+            f = self.func_table[data[0]] 
+            if f.setup != None:
+                f.setup(sender)
+
+
+    '''
+    Taking a snippet of key_val that is length f.num_bytes and then passing it to the decode function
+    returns None
+    parameters:
+        key_val: lst(chr)
+        curr: int
+    '''
+    def parse(self, key_val, curr):
+        while curr < len(key_val):
+
+            f = self.func_table[ord(key_val[curr])]
+            curr += 1
+
+            if f.decoder != None:
+                data = [0] * f.num_bytes
+
+                '''
+                charles did very interesting math for some reason
+                Original:  
+                for(int i = curr+f.num_bytes; curr < i; ++curr) {
+                    data[f.num_bytes - i + curr] = keyVal[curr];
+                
+                curr -> curr + f.num_bytes
+                data[f.num_bytes - f.num_bytes - curr + curr(this is the incremented value)]
+                this is the same as going from 0 -> f.num_bytes
+                key_val[curr -> curr + f.numz_bytes]
+                }
+                '''
+                for i in range(0, f.num_bytes):
+                    data[i] = ord(key_val[i + curr])
+
+                curr += f.num_bytes
+
+                f.decoder(data)
+
+
+    '''
+    returns None
+    parameters:
+        compressed: int
+        compressed_prev: int
+        header: lst(chr)
+    '''
+    def encode(self, compressed, compressed_prev, header):
+        delta = compressed ^ compressed_prev
+
+        expanded = [False] * len(Key_library)
+        for i in range(len(expanded)):
+            expanded[i] = (compressed & (1<<i)) > 0
+    
+        for data in self.func_vector:
+            if data[1] & compressed or data[1] & delta:
+                '''
+                Following line may not work in python
+                We are casting the first field of data which is a char as an index
+                We may need to do something special to avoid issues in python
+                '''
+                f = self.func_table[data[0]]
+                if f.encoder != None:
+                    inputs = []
+                    for k in f.keyboard:
+                        inputs.append(expanded[k.value])
+                    
+                    out = f.encoder(inputs)
+                    if len(out) != f.num_bytes:
+                        print('wrong number of bytes')
+                        continue
+                    
+                    header.append(data[0])
+                    for uc in out:
+                        header.append(uc)
+
+    
+    '''
+    Does a bitwise and to find changes in the keyboard
+    returns bool
+    parameters:
+        compressed: int
+    '''
+    def has_update(self, compressed):
+        return (self.compressed_used & compressed) != 0
