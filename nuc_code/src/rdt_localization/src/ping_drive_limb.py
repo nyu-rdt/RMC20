a34939556f1e37aa5aa13a348206b0dccb167054
trietvuive <vominhtriet02@gmail.com>
2021-01-11 21:27:00 -0500

    pp poo poo

--- nuc_code/src/rdt_localization/src/ping_drive_limb.py
+++ nuc_code/src/rdt_localization/src/ping_drive_limb.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-import rospy
-import paho.mqtt.client as mqtt
-import paho.mqtt.subscribe as subscribe
-
-client = None
 
 from std_msgs.msg import Bool
+
+import rospy
+import paho.mqtt.client as clienttype
+client = None
 
 drive_subsystem_connect = False
 limbs_subsystem_connect = False
@@ -15,23 +14,41 @@ def on_connect(client, userdata, flags, rc):
 
 def drive_ping_received(data):
     global drive_subsystem_connect
+    rospy.loginfo("Drive connected")
     drive_subsystem_connect = True
     
 def limbs_ping_received(data):
     global limbs_subsystem_connect
+    rospy.loginfo("Limbs connected")
     limbs_subsystem_connect = True
+
+def on_message(client, userdata, msg):
+    global drive_subsystem_connect
+    global limbs_subsystem_connect
+    rospy.loginfo("Message received")
+    if msg.topic == "robotCmds/drivePing":
+        drive_subsystem_connect = True
+    elif msg.topic == "robotCmds/limbPing":
+        limbs_subsystem_connect = True
 
 def main():
     global client    
-    rospy.init_node("ping_drive_and_limb")
+    rospy.init_node("ping_drive_limb")
     pub = rospy.Publisher('server/send_drive_vec', Bool, queue_size=10)
-    client = mqtt.Client()
+    
+    client = clienttype.Client()
     client.on_connect = on_connect
-    subscribe.callback(drive_ping_received, "robotState/drivePing", hostname="localhost", port=1883)
-    subscribe.callback(limbs_ping_received, "robotState/limbPing", hostname="localhost", port=1883)
+    client.connect("localhost", 1883)
+
+    client.subscribe("robotCmds/drivePing")
+    client.subscribe("robotCmds/limbPing")
+    
+    #subscribe.callback(drive_ping_received, "robotState/drivePing", hostname="localhost", port=1883)
+    #subscribe.callback(limbs_ping_received, "robotState/limbPing", hostname="localhost", port=1883)
     while not rospy.is_shutdown():
         client.publish("robotCmds/drive", 252, retain=True)
         client.publish("robotCmds/limbs", 252, retain=True)
+        #rospy.loginfo("loop de loop")
         if drive_subsystem_connect and limbs_subsystem_connect:
             pub.publish(True)
         client.loop()
