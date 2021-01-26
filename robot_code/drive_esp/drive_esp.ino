8ed3550163b55057c30af3a1127a2e6aa2456247
DanShafman <dshafman0900@bths.edu>
2021-01-25 21:53:13 -0500

    Updated and cleaned up Arduino code after testing session

--- robot_code/drive_esp/drive_esp.ino
+++ robot_code/drive_esp/drive_esp.ino
@@ -1,38 +1,26 @@
 /*
- * Changelog
- * Changed functions to have better names
- * Imlpemented proper char* casting in scanForCmd()
- */
-
-/* NYU RDT
- * Drive Control code
- * 7    - bit string will be sent from the topic that we the esp is subscribed to
-FOR DRIVE 
-7bits
-0 = mode
-  0- forwards/backwards
-  1- rotating
-  2- arm movement 
-1-3 = speed
-  0- forwards/backwards
-  1- percentage
-  2- percentage
-4-7 = degrees
-  0-neg/pos
-  1-1st dig
-  2-2nd dig
-  3-3th dig
+ * drive_esp.ino
+ * 
+ * Code for the radio in the locomotion subsystem, particularly for a NodeMCU with an ESP8266 
+ * module. Reads commands from the MQTT network and relays them to the locomotion subsystem
+ * controller. Also provides ping functionality so the state controller can check that its
+ * connection to the subsystem is established.
+ * 
+ * See README for descriptions on the format of the command messages.
+ * 
+ * TODO:
+ * - Implement handlers for special drive modes (>252)
  */
 
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-// Connection information like Wifi credentials and server address
-#define WIFI_SSID "Team_16"
+#define WIFI_SSID "Team_15"
 #define WIFI_PASS "lunabots"
 #define WIFI_CHANNEL 1
-#define SERVER_ADDR "192.168.1.2"
+
+#define SERVER_ADDR "192.168.43.201"
 #define SERVER_PORT 1883
 
 #define TOPIC_NAME_IN "robotCmds/drive"
@@ -40,17 +28,24 @@ FOR DRIVE
 #define MQTT_RECONNECT_TIMEOUT 200
 #define MQTT_READ_TIMEOUT 50
 
+#define CMD_TIMEOUT 1000
+#define NEUTRAL_VAL 100
+
 // Create MQTT client on the ESP
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, SERVER_ADDR, SERVER_PORT, "driveTopic", "");
 Adafruit_MQTT_Subscribe inTopic = Adafruit_MQTT_Subscribe(&mqtt, TOPIC_NAME_IN);
 Adafruit_MQTT_Publish pingTopic = Adafruit_MQTT_Publish(&mqtt, TOPIC_NAME_OUT);
 
+unsigned long timeLastRecv;   // Last time in milliseconds command was received
+bool cmdRecv;                 // Has a command been received this cycle
 
 void setup() {
   Serial.begin(115200);
-
   WiFi.begin(WIFI_SSID, WIFI_PASS, WIFI_CHANNEL);
+
+  timeLastRecv = 0;
+  cmdRecv = false;
 
   //  Wait until ESP is connected to wifi to proceed
   while (WiFi.status() != WL_CONNECTED) {
@@ -62,14 +57,25 @@ void setup() {
 
 
 void loop() {
+  // If no command received for CMD_TIMEOUT ms, start writing zero values to the motors at 
+  // the end of the cycle
+  if (millis() > timeLastRecv + CMD_TIMEOUT) cmdRecv = false;
+
   checkConnection();
   scanForCmd();
+
+  // Write zero values
+  if (!cmdRecv) {
+    Serial.write(255);
+    Serial.write(NEUTRAL_VAL);
+    Serial.write(NEUTRAL_VAL);
+  }
 }
 
 
 // Constantly check the connection and reconnect if it is dropped
 void checkConnection(){
-  if (mqtt.connected()) return; 
+  if (mqtt.connected()) return;
 
   int8_t connectionStatus;
   connectionStatus = mqtt.connect();
@@ -80,24 +86,38 @@ void checkConnection(){
     connectionStatus = mqtt.connect();
   }
 }
+    
 
-
-// Scan for any incoming data on any topic. The `subPtr` object will
-// have to be compared in a switch case to determine the topic name.
+// Scan for any incoming data on any topic. The `subPtr` object will have to be compared in
+// a switch case to determine the topic name.
 void scanForCmd(){
   Adafruit_MQTT_Subscribe* subPtr;
   
   while ((subPtr = mqtt.readSubscription(MQTT_READ_TIMEOUT))){
     if (subPtr == &inTopic){
+      cmdRecv = true;
+      timeLastRecv = millis();
+
+      // Read and translate the last incoming string of bytes
+      // Incoming bytes currently come in the following format:
+      //      0x00  0x00  robotSpeed  offsetNum
       char* command = (char*) inTopic.lastread; 
+      char offsetNum = command[2];
+      char robotSpeed = command[3];
       
       // Check if last message was 'ping' byte; if it was, forward
       // response to ping channel
-      if (*command == 252) {
+      if (offsetNum == 252) {
         pingTopic.publish(1);
       }
-      
-      Serial.write(command);
+      // If the message is not the ping packet, process the offset and the robot speed and 
+      // send the command to the controller. The header byte 255 is used for synchronizing 
+      // Tx and Rx.
+      else {
+        Serial.write(255);
+        Serial.write(robotSpeed);
+        Serial.write(offsetNum);
+      }
     }
   }
 }
--- robot_code/drive_teensy/drive_teensy.ino
+++ robot_code/drive_teensy/drive_teensy.ino
@@ -1,91 +1,217 @@
-/* NYU RDT
- * Drive Control code
- * 7    - bit string will be sent from the topic that we the esp is subscribed to
-FOR DRIVE
-7bits
-0 = mode
-  if this bit is....
-    0- forwards/backwards
-    1- rotating
-    2- arm movement
-1-3 = speed
-  0th index represents forwards/backwards
-    0-forwards
-    1-backwards
-  1st & 2nd index are the percent of total motor power
-4-7 = degrees
-  0th index is negative/positive degree
-    0 is pos
-    1 is negative
-  indexes 1-3 represent each index of degree (eg(270))
+/*
+ * drive_teensy.ino
+ * 
+ * Code for the controller in the locomotion subsystem, particularly the Teensyduino. Takes in
+ * a robot speed and offset byte and writes the command to the motors. Also handles special
+ * drive modes such as turning in place and changing the digging drum elevation.
+ * 
+ * See README for descriptions on the format of the command messages.
+ * 
+ * TODO:
+ * - Implement handlers for drive modes
  */
+
 #include <Servo.h>
 
-Servo Spark;
+#define TEST_MOTOR_SPEED 70
 
-bool writetest = false;
+// Wheels 
+Servo Brown;
+Servo Pink;
+Servo Blue;
+Servo Orange;
 
-int val;
-int val2;
-int comms;
+// Arms
+Servo FrontArm;
+Servo BackArm;
 
-void setup (){
-  val = 0;
-  Spark.attach(3);
-  Serial1.begin(115200);
-  Serial.begin(9600);
+void setup(){
+  Serial1.begin(115200);  // Connection to ESP8266
+  Serial.begin(115200);   // Serial Monitor
+
+  // Attach motors and arms
+  Brown.attach(3);
+  Pink.attach(4);
+  Blue.attach(6);
+  Orange.attach(5);
+  FrontArm.attach(9);
+  BackArm.attach(10);
+  // Hardcode function calling
+  /*  forward();
+   *  backward();
+   *  turnLeft();
+   *  turnRight();
+   *  armsUp();
+   *  armsDown();
+   */
 }
 
+/*  reset()
+ *   This function resets all motor speeds to neutral(not moving)
+ */
+void reset() {
+  Brown.writeMicroseconds(1500);
+  Pink.writeMicroseconds(1500);
+  Blue.writeMicroseconds(1500);
+  Orange.writeMicroseconds(1500);
+  FrontArm.writeMicroseconds(1500);
+  BackArm.writeMicroseconds(1500);
+}
+
+/*  forward()
+ *   This function spins all motors so robot moves in the forward direction
+ */
+void forward(int val, int offset) {
+  float valForward = map(val,0,200,1000,2000);
+ // float offset = map(turn,0,200,1000,2000);
+  float valBackward = map(val,0,200,2000,1000);
+  int i;
+  Serial.println("test");
+  Serial.println(valForward);
+  Serial.println(valBackward);
+  if(offset < 100) {
+  float leftRatio = (offset/200);
+  float rightRatio = (offset)/200;
+
+  Brown.writeMicroseconds(valForward);
+  Pink.writeMicroseconds(valForward);
+  Blue.writeMicroseconds(valBackward*leftRatio);
+  Orange.writeMicroseconds(valBackward*leftRatio);
+  }
+
+  if(offset > 100) {
+  float leftRatio = (offset/200);
+  float rightRatio = (offset)/200;
+
+  Brown.writeMicroseconds(valForward*leftRatio);
+  Pink.writeMicroseconds(valForward*leftRatio);
+  Blue.writeMicroseconds(valBackward);
+  Orange.writeMicroseconds(valBackward);
+  }
+  if (offset == 100){
+
+  float leftRatio = (offset/200);
+  float rightRatio = (offset)/200;
+
+  Brown.writeMicroseconds(valForward);
+  Pink.writeMicroseconds(valForward);
+  Blue.writeMicroseconds(valBackward);
+  Orange.writeMicroseconds(valBackward);
+    }
+  
+
+}
+/*  backward()
+ *   This function spins all motors so robot moves in the backward direction
+ */
+void backward(int val) {
+  float valForward = map(val,0,200,1000,2000);
+  float valBackward = map(val,0,200,2000,1000);
+  Serial.println("test");
+  Serial.println(valForward);
+  Serial.println(valBackward);
+  Brown.writeMicroseconds(valBackward);
+  Pink.writeMicroseconds(valBackward);
+  Blue.writeMicroseconds(valForward);
+  Orange.writeMicroseconds(valForward);
+}
+/*  turnLeft()
+ *   This function spins all motors so robot turns Left
+ */
+void turnLeft(int val) {
+  int valForward = map(val,0,200,1000,2000);
+  int valBackward = map(val,0,200,2000,1000);
+  Brown.writeMicroseconds(valBackward);
+  Pink.writeMicroseconds(valBackward);
+  Blue.writeMicroseconds(valBackward);
+  Orange.writeMicroseconds(valBackward);
+}
+/*  turnRight()
+ *   This function spins all motors so robot turns Right
+ */
+void turnRight(int val) {
+  int valForward = map(val,0,200,1000,2000);
+  int valBackward = map(val,0,200,2000,1000);
+  Brown.writeMicroseconds(valForward);
+  Pink.writeMicroseconds(valForward);
+  Blue.writeMicroseconds(valForward);
+  Orange.writeMicroseconds(valForward);
+}
+/*  armsUp()
+ *   This function spins falcon motors so robot's arm goes up
+ *
+ *   Update -
+ *      Wheels should turn as the arms are going up
+ *      - Tested(In progress)
+ */
+void armsUp() {
+  int valForward = map(TEST_MOTOR_SPEED,-100,100,1000,2000);
+  int valBackward = map(-TEST_MOTOR_SPEED,-100,100,1000,2000);
+  Brown.writeMicroseconds(valForward);
+  Pink.writeMicroseconds(valBackward);
+  Blue.writeMicroseconds(valForward);
+  Orange.writeMicroseconds(valBackward);
+  FrontArm.writeMicroseconds(valBackward);
+  BackArm.writeMicroseconds(valBackward);
+}
+/*  armsDown()
+ *   This function spins all motors so robot turns Right
+ *
+ *   Update -
+ *      Wheels should turn as the arms are going down
+ *      - Tested(In progress)
+ */
+void armsDown() {
+  int valForward = map(TEST_MOTOR_SPEED,-100,100,1000,2000);
+  int valBackward = map(-TEST_MOTOR_SPEED,-100,100,1000,2000);
+  Brown.writeMicroseconds(valBackward);
+  Pink.writeMicroseconds(valForward);
+  Blue.writeMicroseconds(valBackward);
+  Orange.writeMicroseconds(valForward);
+  FrontArm.writeMicroseconds(valForward);
+  BackArm.writeMicroseconds(valForward);
+}
+void brown() {
+  int valForward = map(TEST_MOTOR_SPEED, -100,100,1000,2000);
+  Brown.writeMicroseconds(valForward);
+  }
+void pink(){
+  int valForward = map(TEST_MOTOR_SPEED,-100,100,1000,2000);
+  Pink.writeMicroseconds(valForward);
+  }
+void orange(){
+  int valBackward = map(-TEST_MOTOR_SPEED,-100,100,1000,2000);
+  Orange.writeMicroseconds(valBackward);
+  }
+void blue(){
+  int valBackward = map(-TEST_MOTOR_SPEED,-100,100,1000,2000);
+  Blue.writeMicroseconds(valBackward);
+  }
+/*  Incoming byte
+ *   0 - Wheels(0) / Wheels(1)                      Toggle
+ *   1 - Arms(0) / Arms(1)                          Toggle
+ *   2 - Wheels Backward(0) / Wheels Forward(1)
+ *   3 - Arms Down(0) / Arms Up(1)
+ *   4 -
+ *   5 -
+ *   6 -
+ *   7 -
+ */
 void loop() {
-  
-  if(Serial1.available() > 0){
-     comms = Serial1.read();
-     //][----Serial1.println(comms);
+  if (Serial1.available() > 0) {
+    int inByte = Serial1.read();
+    Serial.println("received");
+    Serial.println(inByte);
+    if (inByte == 255 ){ //synchro byte
+      while(!(Serial1.available())){}
+      int inByte2 = Serial1.read();
+      while(!(Serial1.available())){}
+      int inByte3= Serial1.read();
+      forward(inByte2, inByte3);
+
+    }
+    
+      
   }
-  //if(Serial.available() > 0){
-  //   val = (Serial.parseInt());
-  //}
-  interpret(comms);
-//  delay(15);
 
-  //if (writetest) {
-    //digitalWrite(1, HIGH);
-  }
-}
-
-
-void interpret(int& commandUpdate){
-  switch(commandUpdate[0].toInt()){
-    case '0':
-      drive(commandUpdate.substring(1,7));
-      break;
-    case  '1':
-      //keep one side stationary, the other rotates
-      rotate(commandUpdate.substring(1,3));
-      break;
-    case '3':
-      //slowly rotate wheels in different directions slicing like a hamburger
-      rollUp(commandUpdate.substring(1,3));
-      break; 
-  }
-}
-void drive(String commandUpdate){
-  //if [0] is 0, go forwards, if 1, go backwards
-  //translates degrees into different percentages for the wheels divided like a hotdog
-   val = (commandUpdate.substring(1,4)).toInt();
-
-  if((commandUpdate[0].toInt())==1){
-        val*=-1;
-     }
-
-   val2 = map(val,-100,100,1000,2000);
-   Spark.writeMicroseconds(val2);
-  
-}
-void rotate(String comm){
-  //rotates based upon speed passed, one side of the robot remains stationary, while the other spins
-  //if [0] is 0, go forwards, if 1, go backwards
-}
-void rollUp(String Comm){
-  // arm stuff
 }
